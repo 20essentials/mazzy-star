3676c1ad28a5574ead853457ffd58fb6618362e8
DominicDev <s0l0estudi4r@gmail.com>
2025-12-18 13:09:44 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MAZZY STAR';
+export const nameSongs = [
+	"FADE INTO YOU", /*1*/
+	"QUIET, THE WINTER HARBOR", /*2*/
+	"ISN'T IT TRUE", /*3*/
+	"I'M LESS HERE", /*4*/
+	"IN THE KINGDOM", /*5*/
+	"CALIFORNIA", /*6*/
+	"I’VE GOTTA STOP", /*7*/
+	"DOES SOMEONE HAVE YOUR BABY NOW?", /*8*/
+	"COMMON BURN", /*9*/
+	"SEASONS OF YOUR DAY", /*10*/
+	"LAY MYSELF DOWN", /*11*/
+	"SPARROW", /*12*/
+	"SPOON", /*13*/
+	"FLYING LOW", /*14*/
+	"DISAPPEAR", /*15*/
+	"FLOWERS IN DECEMBER", /*16*/
+	"RHYMES OF AN HOUR", /*17*/
+	"CRY, CRY", /*18*/
+	"TAKE EVERYTHING", /*19*/
+	"STILL COLD", /*20*/
+]
